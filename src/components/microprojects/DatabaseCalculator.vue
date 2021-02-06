aa2157640e66215691a6bb630943234115295614
Larry Hau <>
2021-02-05 21:04:59 -0700

    Adding DB and Instance Calcs

---
+++ src/components/microprojects/DatabaseCalculator.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="databasecalc">
+    <h2>{{ $t("Database Migrations") }}</h2>
+
+    <MicroProjectFeature
+      label="DBs to Migrate to RDS"
+      price-key="database-migrate_db_to_rds"
+      v-model="migrate_db_to_rds_total"
+    />
+    <br /><br />
+    <MicroProjectFeature
+      label="RDS Instances to Copy to RDS"
+      price-key="database-copy_rds_to_rds"
+      v-model="copy_rds_to_rds_total"
+    />
+    <br /><br />
+    <MicroProjectFeatureCheckbox
+      label="Replication?"
+      price-key="database-replica"
+      v-model="database-replica_total"
+    />
+    <MicroProjectFeature
+      label="GB of Data (in chunks of 100)"
+      price-key="database-100_gb_of_data"
+      v-model="database-100_gb_of_data_total"
+    />
+    <br /><br />
+    <h3>{{ $t("Database Migrations Total") }}</h3>
+    <i18n-n :value="database_total" format="currency"></i18n-n>
+  </div>
+</template>
+
+<script>
+import prices from "@/prices/MicroProjects.json";
+import MicroProjectFeature from "./MicroProjectFeature.vue";
+import MicroProjectFeatureCheckbox from "./MicroProjectFeatureCheckbox.vue";
+
+export default {
+  name: "DatabaseCalculator",
+  components: {
+    MicroProjectFeature,
+    MicroProjectFeatureCheckbox,
+  },
+  data() {
+    return {
+      currency: "USD",
+      prices: prices,
+      migrate_db_to_rds_total: 0,
+      copy_rds_to_rds_total: 0,
+      database-replica_total: 0,
+      database-100_gb_of_data_total: 0,
+    };
+  },
+  computed: {
+    database_total() {
+      return (
+        this.create_migrate_db_to_rds_total +
+        this.copy_rds_to_rds_total +
+        this.database-replica_total +
+        this.database-100_gb_of_data_total
+      );
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/microprojects/InstanceCalculator.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="instancecalc">
+    <h2>{{ $t("Instance Creation") }}</h2>
+
+    <MicroProjectFeature
+      label="Instances with Security Groups"
+      price-key="instance-create_instance_in_aws_with_sg"
+      v-model="create_instance_in_aws_with_sg_total"
+    />
+    <br /><br />
+    <MicroProjectFeature
+      label="Load Balancers"
+      price-key="instance-load_balancers"
+      v-model="load_balancers_total"
+    />
+    <br /><br />
+    <MicroProjectFeature
+      label="Number of Applications"
+      price-key="instance-applications"
+      v-model="applications_total"
+    />
+    <MicroProjectFeature
+      label="GB of Data (in chunks of 100)"
+      price-key="instance-100_gb_of_data"
+      v-model="instance-100_gb_of_data_total"
+    />
+    <br /><br />
+    <h3>{{ $t("Instance Creation Total") }}</h3>
+    <i18n-n :value="instance_total" format="currency"></i18n-n>
+  </div>
+</template>
+
+<script>
+import prices from "@/prices/MicroProjects.json";
+import MicroProjectFeature from "./MicroProjectFeature.vue";
+import MicroProjectFeatureCheckbox from "./MicroProjectFeatureCheckbox.vue";
+
+export default {
+  name: "InstanceCalculator",
+  components: {
+    MicroProjectFeature,
+    MicroProjectFeatureCheckbox,
+  },
+  data() {
+    return {
+      currency: "USD",
+      prices: prices,
+      create_instance_in_aws_with_sg_total: 0,
+      load_balancers_total: 0,
+      applications_total: 0,
+      instance-100_gb_of_data_total: 0,
+    };
+  },
+  computed: {
+    database_total() {
+      return (
+        this.create_instance_in_aws_with_sg_total +
+        this.load_balancers_total +
+        this.applications_total +
+        this.instance-100_gb_of_data_total
+      );
+    },
+  },
+  methods: {},
+};
+</script>
+
+<style scoped></style>
